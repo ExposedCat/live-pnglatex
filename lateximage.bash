d8b478f96fc1abf14b3f89eae63e55d70d0c58dc
ExposedCat <44642024+ExposedCat@users.noreply.github.com>
2020-12-17 12:26:18 +0200

    Create comments

--- lateximage.bash
+++ lateximage.bash
@@ -1,8 +1,10 @@
+# To get formula from Zenity window:
 formula="$(zenity --text-info \
 --title="TeX to image" \
 --editable)"
+# To get formula from X clipboard:
 # formula=$(xclip -o)
 tex_formula=$(echo "$formula" | sed -z 's/\n/\\\\/g')
-image=$(bash ~/Shell/pnglatex -d 4000 -f "\\\\$tex_formula")
+image=$(bash ~/sh/pnglatex -d 4000 -f "\\\\$tex_formula")
 xclip -selection clipboard -t image/png -i $image
 rm $image
